10d1dd847e940e6c1373dbdab0a7cb7cb1be755b
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-02-10 18:43:02 -0300

    create engine

--- public/index.js
+++ public/index.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+import createEngine from "../src/engine.js";
+
+const canvas = document.querySelector('#canvas');
+const renderer = new createEngine(canvas);
+
+renderer.init();
---
+++ src/engine.js
@@ -0,0 +1,10 @@
+export default function createEngine(canvas) {
+
+    function init() {
+        console.log('[init]')
+    }
+
+    return {
+        init,
+    }
+}
